b6114655d0652b0781ddc305a6bb7d13a951ea14
Artx Hundiak <ahundiak@gmail.com>
2016-07-20 09:28:49 -0500

    Setup for unit testing.

---
+++ srcServer/ProjectBundle/CeradProjectBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Cerad\ProjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CeradProjectBundle extends Bundle
+{
+}
---
+++ srcServer/ProjectBundle/ProjectBundleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Cerad\ProjectBundle;
+
+use PHPUnit_Framework_TestCase;
+
+class ProjectBundleTest extends PHPUnit_Framework_TestCase
+{
+    public function test1()
+    {
+        $this->assertTrue(true);
+    }
+}
